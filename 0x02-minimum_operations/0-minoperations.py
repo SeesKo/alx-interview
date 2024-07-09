00ba24e8e7c43c9fb8e9db3a471888c465db5c80
SeesKo <seeskosee@seeskome.com>
2024-07-08 21:15:29 -0700

    fewest number of operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -9,15 +9,15 @@ def minOperations(n):
     Calculate the minimum number of operations required to
     make the number 'n' using copy and paste operations.
     """
-    if n <= 0:
+    if n <= 1:
         return 0
 
-    dp = [float('inf')] * (n + 1)
-    dp[1] = 0
+    operations = 0
+    d = 2
+    while n > 1:
+        while n % d == 0:
+            operations += d
+            n //= d
+        d += 1
 
-    for i in range(2, n + 1):
-        for j in range(1, i):
-            if i % j == 0:
-                dp[i] = min(dp[i], dp[j] + i // j)
-
-    return dp[n] if dp[n] != float('inf') else 0
+    return operations
