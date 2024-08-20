4f1960b45653a1ceb28ddbf7fa78c8d8df085ab3
SeesKo <seeskosee@seeskome.com>
2024-08-20 12:26:06 -0700

    fewest number of coins

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -12,15 +12,14 @@ def makeChange(coins, total):
     if total <= 0:
         return 0
 
-    # Initialize list to store the minimum coins needed for each amount
+    # Initialize the dp array with infinity
     dp = [float('inf')] * (total + 1)
     dp[0] = 0
 
-    # Iterate through each coin and update the dp list
+    # Update dp array for each coin
     for coin in coins:
-        for x in range(coin, total + 1):
-            dp[x] = min(dp[x], dp[x - coin] + 1)
+        for amount in range(coin, total + 1):
+            if dp[amount - coin] != float('inf'):
+                dp[amount] = min(dp[amount], dp[amount - coin] + 1)
 
-    # If dp[total] is still infinity
-    # then it's not possible to make that amount
     return dp[total] if dp[total] != float('inf') else -1
